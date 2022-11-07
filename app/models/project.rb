590467fc49af3256cd5342d140eba74166763a4b
TaylorVasquez <tvasquez@uccs.edu>
2022-11-07 21:50:49 +0000

    Rspec lab update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+
+    validates :title, presence: true
+    #validates :title, :description, presence: true
+
+    #validates :title, :description, confirmation: true
+    
 end
