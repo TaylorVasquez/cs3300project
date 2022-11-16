228affc5d8a713d8d0f8582fd011cd555964f4d2
TaylorVasquez <tvasquez@uccs.edu>
2022-11-16 22:17:29 +0000

    Fixed the fail on purpose, should be normal again

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ApplicationRecord
 
     #validates :title, presence: true
-
-    #validates :title, presence: true
-    #validates :description, presence: true
+    
+    validates :title, presence: true
+    validates :description, presence: true
     
     #Project.save => true
     #validates :title, :description, confirmation: true
